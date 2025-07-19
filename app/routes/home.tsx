5d23133319653eb0a6088c177e1a29883e282041
igrisdev <johanalvarezz237@gmail.com>
2025-07-18 22:16:52 -0500

    feat: add dialog component and related UI elements
    - Introduced a new DialogForm component for profile editing.
    - Added button, dialog, input, and label components using Radix UI and custom styles.
    - Updated package.json to include new dependencies for Radix UI and utility libraries.
    - Enhanced tsconfig.json with new path aliases for better module resolution.
    - Created utility functions for class name merging.
    - Added components.json for component configuration and styling.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,25 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home'
+import { DialogDemo } from '~/components/functional/DialogForm'
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    { title: 'Following Days' },
+    { name: 'description', content: 'Welcome to React Router!' },
+  ]
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <>
+      <header>
+        <h1>Following Days</h1>
+      </header>
+      <main>
+        <DialogDemo />
+      </main>
+      <footer>
+        <p>This is the footer.</p>
+      </footer>
+    </>
+  )
 }
